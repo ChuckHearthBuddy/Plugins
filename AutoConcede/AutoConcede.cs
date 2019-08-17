04cfb4874e1ce7ded1dea7752bd057e70771b7af
Chuck Lu <chuck.lu@qq.com>
2019-08-17 22:33:09 +0800

    add auto concede plugin

---
+++ AutoConcede/AutoConcede.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Windows.Controls;
+using Triton.Bot;
+using Triton.Common;
+using Triton.Common.LogUtilities;
+using Triton.Game;
+using log4net;
+using SilverFish.ai;
+
+namespace AutoConcede
+{
+    public class AutoConcede : IPlugin
+    {
+        private static readonly ILog Log = Logger.GetLoggerInstanceForType();
+
+        private int _defeatCount;
+
+        private int _winCount = 1;
+
+        private double _winRateThreshold = 0.2;
+
+        private double _realWinRate = 1;
+
+        public void Start()
+        {
+            Log.DebugFormat("[AutoConcede] Initialize");
+            CustomEventManager.Instance.MulliganStarted += CustomEventManager_MulliganStarted;
+            GameEventManager.GameOver += GameEventManager_GameOver;
+        }
+
+        private void CustomEventManager_MulliganStarted(object sender, EventArgs e)
+        {
+            if (_realWinRate >= _winRateThreshold)
+            {
+                TritonHs.Concede(true);
+            }
+        }
+
+        private void GameEventManager_GameOver(object sender, GameOverEventArgs e)
+        {
+            if (e.Result == GameOverFlag.Victory)
+            {
+                _winCount++;
+
+            }
+            else if (e.Result == GameOverFlag.Defeat)
+            {
+                _defeatCount++;
+            }
+
+            CalculateRealWinRate();
+        }
+
+        private void CalculateRealWinRate()
+        {
+            double totalCount = _winCount + _defeatCount;
+            _realWinRate = _winCount / totalCount;
+			
+			Log.InfoFormat("[AutoConcede]  _winCount = " + _winCount);
+			Log.InfoFormat("[AutoConcede]  _defeatCount = " + _defeatCount);
+			Log.InfoFormat("[AutoConcede]  totalCount = " + totalCount);
+            Log.InfoFormat("[AutoConcede] CalculateRealWinRate _realWinRate = " + _realWinRate);
+        }
+
+        public void Tick()
+        {
+        }
+
+        public void Stop()
+        {
+            CustomEventManager.Instance.MulliganStarted -= CustomEventManager_MulliganStarted;
+            GameEventManager.GameOver -= GameEventManager_GameOver;
+        }
+
+        public string Name => "AutoConcede";
+
+        public string Author => "Chuck Lu";
+
+        public string Description => "Auto concede to reduce the odds.";
+
+        public string Version => "0.1.0";
+
+        public void Initialize()
+        {
+        }
+
+        public void Deinitialize()
+        {
+        }
+
+        public UserControl Control => null;
+
+        public JsonSettings Settings => null;
+
+        public void Enable()
+        {
+			IsEnabled = true;
+			Log.DebugFormat("[AutoConcede] Enabled");
+        }
+
+        public void Disable()
+        {
+			IsEnabled = false;
+			Log.DebugFormat("[AutoConcede] Disabled");
+        }
+
+        public bool IsEnabled { get; private set; }
+    }
+}
